5b7acbc0e6ed8a734aeae7c8483d2147bad59a81
Denise Pendleton <dpendl1@hotmail.com>
2018-04-19 17:47:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
+    
 
   end
 
@@ -33,6 +34,13 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect '/success'
+		# else
+		# 	redirect '/failure'
+		end
   end
 
   get "/failure" do
